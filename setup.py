87683e2e3232f002a0fd48f072881711259e8945
Ralph Meijer <ralphm@ik.nu>
2007-08-22 13:44:33 +0000

    Make this packagable.
    --HG--
    extra : convert_revision : svn%3Ab33ecbfc-034c-dc11-8662-000475d9059e/trunk%404

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2003-2007 Ralph Meijer
+# See LICENSE for details.
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='wokkel',
+      version='0.1.0',
+      description='Twisted Jabber support library',
+      author='Ralph Meijer',
+      author_email='ralphm@ik.nu',
+      maintainer_email='ralphm@ik.nu',
+      url='http://wokkel.ik.nu/',
+      license='MIT',
+      platforms='any',
+      packages=[
+          'wokkel',
+      ],
+)
