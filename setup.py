7cc7c95bc10cce5b639779d7942648625c979645
Ralph Meijer <ralphm@ik.nu>
2012-01-23 16:05:05 +0100

    Release Wokkel 0.7.0.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import setup
 
 setup(name='wokkel',
-      version='0.7.0pre1',
+      version='0.7.0',
       description='Twisted Jabber support library',
       author='Ralph Meijer',
       author_email='ralphm@ik.nu',
